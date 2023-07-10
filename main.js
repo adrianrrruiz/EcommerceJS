699b7bdf424afbb8f15b55cb1eecfb2c93d9c7a4
Adrian Ruiz <adrianruiz0208@gmail.com>
2023-07-10 18:43:26 -0500

    Integrando carrito con index.html

--- main.js
+++ main.js
@@ -2,15 +2,27 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+carritoMenuIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive'); //Inactive other screens
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive'); //Inactive other screens
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    mobileMenu.classList.add('inactive'); //Inactive other screens
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
